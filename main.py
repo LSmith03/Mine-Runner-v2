1fd53010f165cf982488940fe34963b83b97f59c
Logan Smith] <ls1130343@gmail.com>
2024-05-19 02:27:37 -0500

    Changed to be more dynamic with a game functions file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Program: Mine Runner Game
 # Author: Logan Smith
 # Description: A fun game where you avoid the mines as you avoid a chaser.
-# Date Modified: 05/15/2024
+# Date Modified: 05/19/2024
 # Version: 2.0
 
 
@@ -13,10 +13,8 @@ from game_functions import run_menu
 # Future Functionality
 # ================================================================
 # Fix overlapping mines and obstacles X
-# Adjust the static values to be dynamic
-# Add a pause screen
-# Add A game opening screen
-# Clean up the main loop
+# Add A game opening screen X
+# Clean up the main loop X
 # Add images to game
 # Add powerups (see mines around you)
 
